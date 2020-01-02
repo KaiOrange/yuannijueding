61f9b607fe6ec959005f2f729b7ea05a83d40586
Orange <huangkaiking@126.com>
2020-01-02 17:00:50 +0800

    不使用cdn

--- src/store.js
+++ src/store.js
@@ -6,8 +6,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     // 使用CDN用来加速图片等资源的访问速度 如果不使用 那么设置为空字符串就可以了
-    // CDNURL: ""
-    CDNURL: "https://ynjd.cn-bj.ufileos.com"
+    CDNURL: ""
+    // CDNURL: "https://ynjd.cn-bj.ufileos.com"
   },
   mutations: {},
   actions: {}
